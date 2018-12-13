d01f97289cbdf9e94fd4b08fe4fa70d66ace71b6
Peter <peterdramos02@gmail.com>
2018-12-13 15:40:57 -0500

    add store manipulation methods

--- scripts/store.js
+++ scripts/store.js
@@ -1,11 +1,56 @@
 'use strict';
 const store = (function () {
 
+  const addBookmark = function (bookmark) {
+    this.bookmarks.push(bookmark);
+  };
+
+  const findById = function (id) {
+    // find an item in bookmarks array by id and return it
+    return this.bookmarks.find(bookmark => bookmark.id === id);
+  };
+
+  const findAndDelete = function (id) {
+    this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id);
+  };
+
+  const toggleIsEditing = function (id) {
+    const editingBookmark = this.findById(id);
+    editingBookmark.isEditing = !editingBookmark.isEditing;
+  };
+
+  const toggleExpanded = function (id) {
+    const expandedBookmark = this.findById(id);
+    expandedBookmark.expanded = !expandedBookmark.expanded;
+  };
+
+  const setMinRating = function (num) {
+    this.minRating = num;
+  };
+
+  const toggleAdding = function () {
+    this.adding = !this.adding;
+  };
+
+  const toggleLoading = function () {
+    this.loading = !this.loading;
+  };
+
+
+
   return {
     bookmarks: [],
     minRating: null,
     adding: false,
     loading: false,
-    error: '',
-  }
+
+    addBookmark,
+    findById,
+    toggleExpanded,
+    toggleIsEditing,
+    findAndDelete,
+    setMinRating,
+    toggleAdding,
+    toggleLoading,
+  };
 }());
